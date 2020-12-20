498ae55804ce60d226548b2db96a5303ac9b07d1
r08922130 <cp4n3gj9448@gmail.com>
2020-12-20 19:41:27 +0800

    AbstractDriverBasedDataSource

---
+++ AbstractDriverBasedDataSource.py
@@ -0,0 +1,35 @@
+import abc
+from DataSource import DataSource
+
+
+class AbstractDriverBasedDataSource(abc.ABC, DataSource):
+    def __init__(self):
+        self.url = None
+        self.username = None
+        self.password = None
+    
+    def setUrl(self,url):
+        self.url = url
+    def getUrl(self):
+        return self.url
+    
+    def setUsername(self,username):
+        self.username = username
+    def getUsername(self):
+        return self.username
+
+    def setPassword(self,password):
+        self.password = password
+    def getPassword(self):
+        return self.password
+    
+    def getConnection(self, username=None, password=None):
+        if username == None:
+            username = self.username
+        if password == None:
+            password = self.password
+        return self.getConnectionFromDriver(username, password)
+    
+    @abc.abstractmethod
+    def getConnectionFromDriver(self, username=None, password=None):
+        raise NotImplementedError

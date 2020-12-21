0aa71862e6e97aa2777c25a9893333ec77f096d9
Yang <csjinyang0611@gmail.com>
2020-12-21 21:44:02 +0800

    fix inner class indent

--- LazyConnectionDataSourceProxy.py
+++ LazyConnectionDataSourceProxy.py
@@ -36,56 +36,56 @@ class LazyConnectionDataSourceProxy(DelegatingDataSource):
    def getConnection(self):
       return LazyConnectionDataSourceProxy(  )
 
-class LazyConnectionInvocationHandler(object):
-   
-   def __init__(self, username = None, password = None):
-      self.target = None
-      self.autoCommit = defaultAutoCommit()
-      self.transactionIsolation = defaultTransactionIsolation()
-      self.username = username
-      self.password = password
-
-      self.closed = False
-
-   def __getattribute__(self, method):
+   class LazyConnectionInvocationHandler(object):
       
-      # TODO: fill up possible method name 
-      if method == "equals":
-         pass
-      elif method == "hashCode":
-         pass
+      def __init__(self, username = None, password = None):
+         self.target = None
+         self.autoCommit = defaultAutoCommit()
+         self.transactionIsolation = defaultTransactionIsolation()
+         self.username = username
+         self.password = password
 
-      if not self.hasTargetConnection():
-            #TODO: fill up possible method name 
-            if method == "toString":
-               pass
-            elif method == "getAutoCommit":
-               pass
+         self.closed = False
+
+      def __getattribute__(self, method):
+         
+         # TODO: fill up possible method name 
+         if method == "equals":
+            pass
+         elif method == "hashCode":
+            pass
+
+         if not self.hasTargetConnection():
+               #TODO: fill up possible method name 
+               if method == "toString":
+                  pass
+               elif method == "getAutoCommit":
+                  pass
+               else:
+                  if self.closed:
+                     raise Exception("Illegal operation: connection is closed")
+
+         target = self.getTargetConnection(method)
+         attr = object.__getattribute__(target, method)
+
+         def newAttr(*args, **kwargs):  # 包装
+            res = attr(*args, **kwargs)
+            return res
+
+         return newAttr
+
+      def hasTargetConnection(self):
+         return (self.target != None)
+
+      def getTargetConnection(self, method):
+         target = object.__getattribute__(self, "target")
+         if target == None:
+            print("Connecting to database for operation '" + method + "'")
+            # Fetch physical Connection from DataSource.
+            if self.username and self.password:
+               super().obtainTargetDataSource().getConnection(self.username, self.password)
             else:
-               if self.closed:
-                  raise Exception("Illegal operation: connection is closed")
-
-      target = self.getTargetConnection(method)
-      attr = object.__getattribute__(target, method)
-
-      def newAttr(*args, **kwargs):  # 包装
-         res = attr(*args, **kwargs)
-         return res
-
-      return newAttr
-
-   def hasTargetConnection(self):
-      return (self.target != None)
-
-   def getTargetConnection(self, method):
-      target = object.__getattribute__(self, "target")
-      if target == None:
-         print("Connecting to database for operation '" + method + "'")
-         # Fetch physical Connection from DataSource.
-         if self.username and self.password:
-            super().obtainTargetDataSource().getConnection(self.username, self.password)
-         else:
-            super().obtainTargetDataSource().getConnection()
-      
-      # TODO: set other connection metadata
+               super().obtainTargetDataSource().getConnection()
+         
+         # TODO: set other connection metadata
      

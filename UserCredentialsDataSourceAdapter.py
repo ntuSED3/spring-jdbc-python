845a3e945cebcbc5f8f3b3a472bfee1f59d6c647
tpainting <caleb860513@gmail.com>
2020-12-22 16:57:02 +0800

    Fix bugs

--- UserCredentialsDataSourceAdapter.py
+++ UserCredentialsDataSourceAdapter.py
@@ -4,6 +4,8 @@ import threading
 
 class UserCredentialsDataSourceAdapter (DelegatingDataSource):
     JdbcUserCredentials = threading.local()
+    JdbcUserCredentials.username = None
+    JdbcUserCredentials.password = None
     _username = None
     _password = None
     _catalog = None
@@ -33,21 +35,20 @@ class UserCredentialsDataSourceAdapter (DelegatingDataSource):
         self.JdbcUserCredentials.username = None
         self.JdbcUserCredentials.password = None
 
-    def getConnection(self):
-        con = doGetConnection(self.JdbcUserCredentials.username, self.JdbcUserCredentials.password) \
-                if JdbcUserCredentials.username is not None and JdbcUserCredentials.password is not None \
-                else doGetConnection(self._username, self._password)
+    def getConnection(self, username=None, password=None):
+        if username == None or password == None:
+            con = self.doGetConnection(self.JdbcUserCredentials.username, self.JdbcUserCredentials.password) \
+                    if self.JdbcUserCredentials.username is not None and self.JdbcUserCredentials.password is not None \
+                    else self.doGetConnection(self._username, self._password)
 
-        if (self._catalog is not None):
-            con.setCatalog(self._catalog)
+            if (self._catalog is not None):
+                con.setCatalog(self._catalog)
 
-        if (self._schema is not None):
-            con.setSchema(self._schema)
-
-        return con
-
-    def getConnection(self, username: str, password: str):
-        return doGetConnection(username, password)
+            if (self._schema is not None):
+                con.setSchema(self._schema)
+            return con
+        else:
+            return self.doGetConnection(username, password)
 
     def doGetConnection(self, username: str, password: str):
         assert super().getTargetDataSource() is not None

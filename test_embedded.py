93bbba06282de366d766f6e0103f47058fa57200
e23099 <e23099@gmail.com>
2020-12-22 19:31:57 +0800

    add test_embedded.py

---
+++ test_embedded.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from embedded.EmbeddedDatabaseFactory import EmbeddedDatabaseFactory
+
+
+if __name__ == "__main__":
+
+    ds = EmbeddedDatabaseFactory().getDatabase()
+    conn = ds.getConnection()
+
+    cur = conn.cursor()
+    cur.execute("""DROP TABLE IF EXISTS customers;""")
+    cur.execute("""CREATE TABLE customers(
+            id INT, first_name VARCHAR(255), last_name VARCHAR(255));""")
+    cur.execute("""INSERT INTO customers VALUES (1, 'Andy', 'Wang');""")
+    cur.execute("""SELECT * FROM customers;""")
+    records = cur.fetchall()
+    df = pd.DataFrame(records)
+    print(df)
+    

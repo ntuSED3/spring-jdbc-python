cb5fdd60813173b400668834e3c93572bffe47ef
e23099 <e23099@gmail.com>
2020-12-23 22:09:46 +0800

    H2Configurer url update

--- embedded/H2Configurer.py
+++ embedded/H2Configurer.py
@@ -19,5 +19,5 @@ class H2Configurer(EmbeddedDatabaseConfigurer):
 
     def configure(self, cp, databaseName):
         cp.setJClassName("org.h2.Driver")
-        cp.setUrl("jdbc:h2:mem:{};DB_CLOSE_DELAY=-1".format(databaseName))
+        cp.setUrl("jdbc:h2:mem:{};DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false".format(databaseName))
         cp.setJarPath("embedded/jar/h2-1.4.200.jar")

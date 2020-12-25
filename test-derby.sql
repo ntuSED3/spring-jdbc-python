0de661a8cf4959098239a834e5287332f69528eb
e23099 <e23099@gmail.com>
2020-12-25 20:42:07 +0800

    update embedded test case

---
+++ test-derby.sql
@@ -0,0 +1,3 @@
+CREATE TABLE customers(id INT, first_name VARCHAR(255), last_name VARCHAR(255))
+INSERT INTO customers VALUES (1, 'Andy', 'Wang')
+INSERT INTO customers VALUES (2, 'Sherry', 'Liu')
--- test_embedded.py
+++ test_embedded.py
@@ -5,43 +5,38 @@ from database.embedded.EmbeddedDatabaseType import EmbeddedDatabaseType
 builder = EmbeddedDatabaseBuilder()
 
 print('=====test builder/HSQL=====')
-hsql_ds = builder.\
-    setType(EmbeddedDatabaseType.HSQL).\
-    addScript("test.sql").\
-    addScript("test2.sql").\
-    generateUniqueName(True).\
-    build()
+hsql_ds = (builder
+    .setType(EmbeddedDatabaseType.HSQL)
+    .addScript("test.sql")
+    .addScript("test2.sql")
+    .generateUniqueName(True)
+    .build()
+)
 jdbcTemplate = JdbcTemplate(hsql_ds)
 print(jdbcTemplate.query("SELECT * FROM customers;"))
 hsql_ds.shutdown()
 print('=====end=====\n')
 
 print('=====test builder/H2=====')
-h2_ds = builder.\
-    setType(EmbeddedDatabaseType.H2).\
-    addScript("test.sql").\
-    addScript("test2.sql").\
-    generateUniqueName(True).\
-    build()
+h2_ds = (builder
+    .setType(EmbeddedDatabaseType.H2)
+    .addScript("test.sql")
+    .addScript("test2.sql")
+    .generateUniqueName(True)
+    .build()
+)
 jdbcTemplate = JdbcTemplate(h2_ds)
 print(jdbcTemplate.query("SELECT * FROM customers;"))
 h2_ds.shutdown()
 print('=====end=====\n')
 
 print('=====test Derby======')
-derby_ds = builder.setType(EmbeddedDatabaseType.DERBY).build()
+derby_ds = (builder
+    .setType(EmbeddedDatabaseType.DERBY)
+    .addScript("test-derby.sql")
+    .build()
+)
 jdbcTemplate.SetDataSource(derby_ds)
-jdbcTemplate.execute('''
-    CREATE TABLE SEDGROUP(
-    ID      INT         PRIMARY KEY,
-    NAME    VARCHAR(12))
-    ''')
-jdbcTemplate.execute('''
-INSERT INTO SEDGROUP VALUES (3, 'Group3')
-''')
-jdbcTemplate.update('''
-UPDATE SEDGROUP set NAME = 'BEST TEAM' where ID=3
-''')
-print(jdbcTemplate.query("SELECT * FROM SEDGROUP"))
+print(jdbcTemplate.query("SELECT * FROM CUSTOMERS"))
 derby_ds.shutdown()
 print('=====end=====\n')
